c972c0d947db57b27ccb248d2564b5209fa58788
Luke Jennings <lukejennings123@gmail.com>
2022-11-07 18:49:16 +0000

    completed animation of the title

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,6 @@
+import React from 'react';
 import Onload from "./onload";
-
+import "./index.css"
 
 function App() {
   return (
--- portfolio/src/onload.js
+++ portfolio/src/onload.js
@@ -1,11 +1,16 @@
+import './index.css';
+
 
 function Onload() {
 
 
     return (
-    <>
-    <div>hi</div>
-    </>
+    <div className = 'onload-wrapper'>
+    <h1 id = 'name'>Luke Jennings</h1>
+    <h2 id = 'info'>Full-Stack Dev | Engineering Graduate
+    <div id = 'info-block'></div>
+    </h2>
+    </div>
     )
 }
 
