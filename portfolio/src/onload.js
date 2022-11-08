7c7754f8efc901d7c33596c2014ed6ad67b13523
Luke Jennings <lukejennings123@gmail.com>
2022-11-08 01:17:16 +0000

    added scroll icon at the bottom of page. Animated it to ease in and move up and down to prompt user to scroll

--- portfolio/src/onload.js
+++ portfolio/src/onload.js
@@ -1,7 +1,10 @@
 import './index.css';
-
+import scrollIcon from './assets/scroll.png'
+import { useRef } from 'react'
 
 function Onload() {
+
+
 
 
     return (
@@ -10,6 +13,7 @@ function Onload() {
     <h2 id = 'info'>Full-Stack Dev | Engineering Graduate
     <div id = 'info-block'></div>
     </h2>
+    <img className = 'scrollIcon' id = 'scroll-down-icon' src={scrollIcon}></img>
     </div>
     )
 }
