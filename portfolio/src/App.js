380389d346e8d1eb1b186183dbd3bcd92b0285e5
Luke Jennings <lukejennings123@gmail.com>
2022-11-15 15:05:44 +0000

    added page book mark

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,13 +3,21 @@ import Onload from "./onload";
 import Aboutme from './aboutme';
 import Projects from './projects';
 import ContactMe from './contact';
+import Myskills from './myskills';
 import "./index.css"
+import aboutMeIcon from './assets/aboutmeIcon.png'
+import { useInView } from 'react-intersection-observer';
+import { useRef, useState } from 'react';
+
 
 function App() {
+
+
   return (
     <div className="site-container">
       <Onload/>
       <Aboutme />
+            {/* <div id = 'enter-bookmark' ref={aboutMeRef}></div> */}
       <Projects />
       <ContactMe />
   </div>
--- portfolio/src/aboutme.js
+++ portfolio/src/aboutme.js
@@ -2,15 +2,18 @@ import './index.css'
 import { useInView } from 'react-intersection-observer';
 import { useRef, useState } from 'react';
 import Myskills from './myskills';
+import aboutMeIcon from './assets/aboutmeIcon.png'
+
 
 
 function Aboutme() {
     const { ref, inView, entry } = useInView(); // this is the useInView component. It takes a ref and declares true 
                                                // when the element with the attached ref is in frame of the browser window.
                                               // The inview is the state IE True/False and entry is the event info. 
-    let aboutMeAnimation1 = useRef()
-    let aboutMeAnimation2 = useRef()
-    let aboutMeAnimation3 = useRef()
+    const aboutMeAnimation1 = useRef()
+    const aboutMeAnimation2 = useRef()
+    const aboutMeAnimation3 = useRef()
+    const fadeInAnimation = useRef()
 
     let isInView = null
     if (inView === true) {
@@ -24,20 +27,30 @@ function Aboutme() {
          aboutMeAnimation1.current.id += 'about-me-text-content-animation'
          aboutMeAnimation2.current.id += 'about-me-text-content-animation2'
          aboutMeAnimation3.current.id += 'about-me-text-content-animation3'
+         fadeInAnimation.current.className = 'bookMark-wrapper fadeAnimation'
      }
  
 
 
     return (
     <div id = 'about-me-wrapper'>
+        <div className='bookMark-wrapper' ref={fadeInAnimation}>
+                <img className = 'bookMark' id = 'bookmark1' src={aboutMeIcon}></img>
+                <img className = 'bookMark' id = 'bookmark2' src={aboutMeIcon}></img>
+                <img className = 'bookMark' id = 'bookmark3' src={aboutMeIcon}></img>
+                <img className = 'bookMark' id = 'bookmark4' src={aboutMeIcon}></img>
+                <img className = 'bookMark' id = 'bookmark5' src={aboutMeIcon}></img>
+      </div>
             <h2 id = 'about-me-page-title' ref={ref}>
+            
+                
             <div ref = {fadeRight} id = 'fade-right-about-me'>-</div>
                About Me
              <div ref = {fadeLeft} id = 'fade-left-about-me'>-</div>
              </h2>
             <p className='about-me-text-content' ref={aboutMeAnimation1} >Hi! I'm Luke...and I love building things!</p>
             <p></p>
-            <p className='about-me-text-content' ref={aboutMeAnimation2} >From building my first computer at 12 years old to graduating with an electrical engineering degree I have always
+            <p className='about-me-text-content' ref={aboutMeAnimation2} >From building my first computer at as a kid to graduating with an electrical engineering degree I have always
                 been curious about how things work "under the hood". My love of development grew from using low-level languages (C++/C) in my degree 
                 where I found myself being drawn more to the design and UI/UX aspect of software development.  </p>
                 <p></p>
